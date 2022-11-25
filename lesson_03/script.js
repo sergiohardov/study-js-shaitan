fee68d64337bad39dae8ef762723e99d39aeb503
Sergio Hardov <sergiohardov@gmail.com>
2022-11-25 21:17:19 +0200

    Готовая домашка 03

---
+++ lesson_03/script.js
@@ -0,0 +1,20 @@
+let a = +prompt("Enter first value:", "");
+let b = +prompt("Enter second value:", "");
+
+function sum(a, b) {
+  console.log(a + b);
+}
+function sub(a, b) {
+  console.log(a - b);
+}
+function mult(a, b) {
+  console.log(a * b);
+}
+function div(a, b) {
+  console.log(a / b);
+}
+
+sum(a, b);
+sub(a, b);
+mult(a, b);
+div(a, b);
