3b4bfdcdc357279f3fe24b7d687a130b6ad190a8
Sergio Hardov <sergiohardov@gmail.com>
2022-11-25 21:05:33 +0200

    Готовая домашка 02

---
+++ lesson_02/script.js
@@ -0,0 +1,15 @@
+// Task #1
+let admin = "";
+let name = "";
+
+name = "Джон";
+admin = name;
+alert(admin);
+
+// Task #2
+let planet = "Earth";
+let currentUser;
+
+// Task #3
+const numeric = "10" + 10;
+console.log(typeof numeric);
