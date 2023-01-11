3d80f2342de2b573ae6acbd9884c91be28c6373b
Sergio Hardov <sergiohardov@gmail.com>
2023-01-11 19:46:02 +0200

    Готовая домашка 19

--- lesson_19/script.js
+++ lesson_19/script.js
@@ -5,11 +5,11 @@ const foo = [1, 2, 3, -1, -2, -3];
 function get_positive_numbers(arr) {
   if (arr.length === 0) return "Array is empty";
 
-  const exampleArr = [];
+  const resultArr = [];
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) exampleArr.push(arr[i]);
+    if (arr[i] > 0) resultArr.push(arr[i]);
   }
 
-  return exampleArr.length > 0 ? exampleArr : null;
+  return resultArr.length > 0 ? resultArr : null;
 }
