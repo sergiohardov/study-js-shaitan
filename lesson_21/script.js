642b6460392b468d72e22a2929f066e12ce70b32
Sergio Hardov <sergiohardov@gmail.com>
2023-02-01 15:24:47 +0200

    Готовая домашка 21 (доп.задание)

--- lesson_21/script.js
+++ lesson_21/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 const mainFunction = (callback) => {
   const firstNum = +prompt("Введите первое число:", "");
   const secondNum = +prompt("Введите второе число:", "");
@@ -13,4 +12,22 @@ const exponentiation = (numA, numB) => {
   alert(result);
 };
 
+const multiplay = (numA, numB) => {
+  const result = numA * numB;
+  alert(result);
+};
+
+const division = (numA, numB) => {
+  const result = numA / numB;
+  alert(result);
+};
+
+const modulo = (numA, numB) => {
+  const result = numA % numB;
+  alert(result);
+};
+
 mainFunction(exponentiation);
+mainFunction(multiplay);
+mainFunction(division);
+mainFunction(modulo);
