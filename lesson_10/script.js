9d5294e7e4201faee62c3e0a831d110c75fc6a46
Sergio Hardov <sergiohardov@gmail.com>
2023-02-13 13:22:32 +0200

    Готовая домашка 10

--- lesson_10/script.js
+++ lesson_10/script.js
@@ -3,23 +3,17 @@
 let numOrStr = prompt("input number or string");
 console.log(numOrStr);
 
-// numOrStr.trim() === ""
-// isNaN(+numOrStr)
-
-// switch (numOrStr) {
-//   case null:
-//     console.log("вы отменили");
-//     break;
-
-//   case "":
-//     console.log("Empty String");
-//     break;
-
-//   case isNaN():
-//     console.log(" number is Ba_NaN");
-//     break;
-
-//   default:
-//     console.log("OK!");
-//     break;
-// }
+switch (numOrStr) {
+  case null:
+    console.log("вы отменили");
+    break;
+  case "":
+    console.log("Empty String");
+    break;
+  case String(Number(numOrStr)):
+    console.log("OK!");
+    break;
+  default:
+    console.log("number is Ba_NaN");
+    break;
+}
