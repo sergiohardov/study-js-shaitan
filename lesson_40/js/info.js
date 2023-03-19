cffb0f7d35c9a5cab6f3a45286530b71ed588963
Sergio Hardov <sergiohardov@gmail.com>
2023-03-19 15:34:14 +0200

    Готовая домашка 40

--- lesson_40/js/info.js
+++ lesson_40/js/info.js
@@ -3,17 +3,22 @@
 (function () {
   document.addEventListener("DOMContentLoaded", () => {
     let data = localStorage.getItem(DATA_KEY);
-    if (!data) return;
 
-    data = JSON.parse(data);
-    const ul = document.createElement("ul");
+    if (!data) {
+      const p = document.createElement("p");
+      p.innerHTML = "Данных еще нет...";
+      document.body.append(p);
+    } else {
+      data = JSON.parse(data);
+      const ul = document.createElement("ul");
 
-    Object.keys(data).forEach((key) => {
-      const li = document.createElement("li");
-      li.innerHTML = `${key}: ${data[key]}`;
-      ul.append(li);
-    });
-    
-    document.body.append(ul);
+      Object.keys(data).forEach((key) => {
+        const li = document.createElement("li");
+        li.innerHTML = `${key}: ${data[key]}`;
+        ul.append(li);
+      });
+
+      document.body.append(ul);
+    }
   });
 })();
